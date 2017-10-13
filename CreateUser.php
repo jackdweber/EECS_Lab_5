c7f1b90620243d87b98f5e6c886277832ada4a45
Weber <j800w308@cycle2.eecs.ku.edu>
2017-10-12 20:24:26 -0500

    Finished user creation.

--- CreateUser.php
+++ CreateUser.php
@@ -8,7 +8,7 @@ echo $user_id;
 #Connect to database
 $servername = "mysql.eecs.ku.edu:3306";
 $username = "jweber";
-$password = "P@$$word123";
+$password = 'P@$$word123';
 $db = "jweber";
 
 // Create connection
@@ -22,14 +22,14 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM Users WHERE user_id ='".$user_id."'";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
+if ($result->num_rows <= 0 && $user_id != '') {
     $sql = "INSERT INTO Users (user_id) VALUES ('".$user_id."')";
     if($conn->query($sql) === TRUE){
         echo "Success";
     }
 }
 else{
-    echo 'User already exists.';
+    echo 'User already exists or not enough info.';
 }
 $conn->close();
 
